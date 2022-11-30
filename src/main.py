1360cab9d784df0866af1bef1cb9f5b27a92951f
jpcampos <jpcampos@inegi.up.pt>
2022-11-30 09:52:36 +0000

    Change from http server to local method

--- src/main.py
+++ src/main.py
@@ -1,96 +1,69 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
 import subprocess
+import json
 from defines import MAIN_BKP, QCOW_ORG
 
 
-class ServerHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        global G_UID, G_MACHINES, G_GPUS, G_STORAGE
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        req = json.loads(post_data)
-        try:
-            self.process_req(req['type'], req['name'], req['ram'],
-                             req['cpus'], req['gpus'], req['storage'])
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json.dumps(
-                {'name': f'vm{G_UID-1}'}).encode('utf-8'))
-        except BaseException as e:
-            self.send_response(400)
-            self.end_headers()
-            print(e)
-
-    def process_req(self, type, name, ram, cpus, gpus, storage):
-        global G_UID, G_MACHINES, G_GPUS, G_STORAGE
-        if type == 0:
-            self.create_machine(ram, cpus, gpus, storage)
-        else:
-            self.destroy_machine(name)
-        with open(MAIN_BKP, 'w') as file:
-            file.write(json.dumps(
-                {'uid': G_UID, 'machines': G_MACHINES, 'gpus': G_GPUS, 'storage': G_STORAGE}))
-
-    def attach_dev(self, dict, max, vmid):
-        arr = [x for x in dict if dict[x]]
-        if len(arr) > max:
-            arr = arr[:max]
-        for x in arr:
-            subprocess.run(
-                f'virsh attach-device {vmid} {x} --config', shell=True)
-            dict[x] = False
-        return arr
-
-    def create_machine(self, ram, cpus, gpus, storage):
-        global G_UID, G_MACHINES, G_GPUS, G_STORAGE
-        vmid = f'vm{G_UID}'
-        subprocess.run(
-            f'cp {QCOW_ORG} /var/lib/libvirt/images/{vmid}.qcow2', shell=True)
-        subprocess.run(
-            f'virt-install --name {vmid} --virt-type kvm --hvm --memory {ram * 1024} \
-            --vcpus {cpus} --disk /var/lib/libvirt/images/{vmid}.qcow2,format=qcow2 \
-            --network network=default --graphics vnc,listen=0.0.0.0 \
-            --noautoconsole --os-variant=ubuntu20.04 --import', shell=True)
-
-        used_gpus = self.attach_dev(G_GPUS, gpus, vmid)
-        used_storage = self.attach_dev(G_STORAGE, storage, vmid)
-
-        G_UID = G_UID + 1
-        G_MACHINES[vmid] = {
-            'gpus': used_gpus,
-            'storage': used_storage
-        }
-
-        subprocess.run(f'virsh destroy {vmid}', shell=True)
-        subprocess.run(f'virsh start {vmid}', shell=True)
-
-    def destroy_machine(self, name):
-        global G_UID, G_MACHINES, G_GPUS, G_STORAGE
-        machine = G_MACHINES[name]
-        for x in machine['gpus']:
-            G_GPUS[x] = True
-        for x in machine['storage']:
-            G_STORAGE[x] = True
-        subprocess.run(f'virsh destroy {name}', shell=True)
-        subprocess.run(f'virsh undefine {name}', shell=True)
-        subprocess.run(f'rm /var/lib/libvirt/images/{name}.qcow2', shell=True)
+UID: int = 1
+MACHINES: dict = {}
+GPUS: dict = {}
+STORAGE: dict = {}
 
 
-ADDR = ('192.168.190.190', 58580)
-HTTP = HTTPServer(ADDR, ServerHandler)
-
-G_UID = 1
-G_MACHINES = {}
-G_GPUS = {}
-G_STORAGE = {}
-
-with open(MAIN_BKP, 'r') as file:
+with open(MAIN_BKP,'r') as file:
     obj = json.load(file)
-    G_UID = obj['uid']
-    G_MACHINES = obj['machines']
-    G_GPUS = obj['gpus']
-    G_STORAGE = obj['storage']
+    UID = obj['uid']
+    MACHINES = obj['machines']
+    GPUS = obj['gpus']
+    STORAGE = obj['storage']
 
-HTTP.serve_forever()
+
+def update_file() -> None:
+    global UID, MACHINES, GPUS, STORAGE
+    with open(MAIN_BKP,'w') as file:
+        json.dump({ 'uid': UID, 'machines': MACHINES, 'gpus': GPUS, 'storage': STORAGE }, file)
+
+
+
+def attach_devices(devices: dict, max: int, machine: str) -> list:
+    arr = [x for x in devices if devices[x]]
+    if len(arr) > max:arr = arr[:max]
+    for x in arr: subprocess.run(f'virsh attach-device {machine} {x} --config', shell=True)
+    return arr
+
+
+def create_machine(ram: int, cpus: int, gpus: int, storage: int) -> str:
+    global UID, MACHINES, GPUS, STORAGE
+    machine = {'gpus': [], 'storage': []}
+    vmid = f'vm{UID}'
+    subprocess.run(
+        f'cp {QCOW_ORG} /var/lib/libvirt/images/{vmid}.qcow2', shell=True)
+    subprocess.run(
+        f'virt-install --name {vmid} --virt-type kvm --hvm --memory {ram * 1024} \
+        --vcpus {cpus} --disk /var/lib/libvirt/images/{vmid}.qcow2,format=qcow2 \
+        --network network=default --graphics vnc,listen=0.0.0.0 \
+        --noautoconsole --os-variant=ubuntu20.04 --import', shell=True)
+
+    used_gpus = attach_devices(GPUS, gpus, vmid)
+    used_storage = attach_devices(STORAGE, storage, vmid)
+    for i in used_gpus: GPUS[i] = False
+    for i in used_storage: STORAGE[i] = False
+    machine['gpus'] = used_gpus
+    machine['storage'] = used_storage
+
+    subprocess.run(f'virsh destroy {vmid}', shell=True)
+    subprocess.run(f'virsh start {vmid}', shell=True)
+
+    UID = UID + 1
+    MACHINES[vmid] = machine
+    update_file()
+
+
+def destroy_machine(name: str) -> None:
+    global UID, MACHINES, GPUS, STORAGE
+    machine = MACHINES[name]
+    for x in machine['gpus']: GPUS[x] = True
+    for x in machine['storage']: STORAGE[x] = True
+    subprocess.run(f'virsh destroy {name}', shell=True)
+    subprocess.run(f'virsh undefine {name}', shell=True)
+    subprocess.run(f'rm /var/lib/libvirt/images/{name}.qcow2', shell=True)
+    update_file()
--- src/query_sharepoint.py
+++ src/query_sharepoint.py
@@ -1,24 +1,26 @@
 from time import sleep
 import json
-import requests
 from datetime import datetime, timezone, timedelta
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
 from defines import SP_USER, SP_PASS, QURY_BKP
+from main import create_machine,destroy_machine
 
 
 PREFIX = 'inegiuppt'
 SITE = 'inegi_compute'
 SITEURL = f'https://{PREFIX}.sharepoint.com/sites/{SITE}'
 SP_LIST = 'Requests'
-PARSED_IDS = {}
+PARSED_IDS: dict = {}
+
+
+with open(QURY_BKP, 'r') as file:
+    PARSED_IDS = json.load(file)
 
 
 def isToday(date: str) -> bool: return datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() == datetime.today().date()
-def isYeste(date: str) -> bool: return datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() == datetime.today().date() - timedelta(days=1)
+def isYesterday(date: str) -> bool: return datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() == datetime.today().date() - timedelta(days=1)
 
-
-with open(QURY_BKP, 'r') as read_file: PARSED_IDS = json.load(read_file)
 
 while True:
     sleep(10)
@@ -29,28 +31,22 @@ while True:
         ctx.load(sp_items)
         ctx.execute_query()
         active_items = [i.properties for i in sp_items if i.properties['Active']]
+        new_items = [i for i in active_items if isToday(i['From']) and i['ID'] not in PARSED_IDS]
+        old_items = [i for i in active_items if isYesterday(i['To']) and i['ID'] in PARSED_IDS]
 
-        for i in active_items:
-            id, name = str(i['ID']), ''
-            msgtype, r, g, c, s = None, 0, 0, 0, 0
-            if id in PARSED_IDS and isYeste(i['To']):
-                if not PARSED_IDS[id]['destroyed']:
-                    name = PARSED_IDS[id]['name']
-                    msgtype, r, g, c, s = 1, 0, 0, 0, 0
-            if id not in PARSED_IDS and isToday(i['From']):
-                msgtype, r, g, c, s = 0, i['RAM_x0028_32GB_x0029_'] * 32, i['GPUS'], i['GPUS'] * 4, i['Storage_x0028_2TB_x0029_']
-            if type != None:
-                req = requests.post('http://192.168.190.190:58580', json={
-                    'type': msgtype,
-                    'name': name,
-                    'ram': r,
-                    'gpus': g,
-                    'cpus': c,
-                    'storage': s
-                })
-                if msgtype == 0: name = req.json()['name']
-                PARSED_IDS[id] = {'name': name, 'destroyed': False if msgtype == 0 else True}
-                with open(QURY_BKP, 'w') as write_file: json.dump(PARSED_IDS, write_file)
+        for i in new_items:
+            name = create_machine(i['RAM_x0028_32GB_x0029_'] * 32, i['GPUS'] * 4, i['GPUS'], i['Storage_x0028_2TB_x0029_'])
+            PARSED_IDS[i['ID']] = { 'name': name, 'destroyed': False }
+
+        for i in old_items:
+            name = PARSED_IDS[i['ID']]['name']
+            destroy_machine(name)
+            PARSED_IDS[i['ID']] = { 'name': name, 'destroyed': True }
+
+        if len(new_items) + len(old_items) > 0:
+            with open(QURY_BKP, 'w') as file:
+                json.dump(PARSED_IDS, file)
+
     except Exception as ex:
         print(f'Error {ex}')
         break
